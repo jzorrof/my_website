c7b8e616b9c2084034bd4b7c95be9c9bf43c4a01
jzorrof@gmail.com <jzorrof@gmail.com>
2015-05-06 16:19:54 +0800

    add module.py

---
+++ module.py
@@ -0,0 +1,30 @@
+# -*- coding: uft-8 -*-
+__author__ = 'joe_fan'
+"""
+mongodb module
+"""
+URL = ''
+
+from pymongo import MongoClient
+from pymongo import ASCENDING, DESCENDING
+
+def dbconnction(url=''):
+    client = MongoClient(url)
+    return client
+
+def dbqurey():
+    db =dbconnction(URL)
+    cursor = db.restaurants.find().sort(
+    [
+        ("borough", ASCENDING),
+        ("address.zipcode", DESCENDING)
+    ]
+    )
+    return cursor
+
+cursor = dbqurey()
+
+for i in cursor:
+    print i
+
+
