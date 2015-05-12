7d439e9b13a521badcaad1234e2f681fe5668bfa
fanzhong <zhongfan@lewatek.com>
2015-05-12 17:03:35 +0800

    upadte qiche.html
    work with S3

--- mywebsite.py
+++ mywebsite.py
@@ -13,7 +13,7 @@ I'll create my website from now
 data: 2015.04.10
 '''
 def get_from_s3():
-    conn = S3Connection('AKIAJZ5NU5RXHVW3QXPA', 'dHE5tDMMk/WwAoyvrd44TaKsJfnNqLSjEUGOmXt5')
+    conn = S3Connection()
     bucketname = conn.get_bucket('scrapy_data_2')
     print bucketname
     k = Key(bucketname)
@@ -31,13 +31,14 @@ def qiche():
         with open("getjson.json") as jsf:
             for each_line in jsf:
                 js = json.loads(each_line,encoding='utf-8')
-                getjson = json.dumps(js, ensure_ascii=False)
-                print(type(getjson))
+                testdata.append(js['desc'][0])
+                #getjson = json.dumps(js, ensure_ascii=False)
+                #print(getjson)
     except IOError as err:
         print('err was' + str(err))
         #return render_template('qiche.html' , testdata={'error':'nothingloaded'})
     return render_template('qiche.html' , testdata=testdata)
 
 if __name__ == '__main__':
-    #get_from_s3()
+    get_from_s3()
     app.run(debug = True)
