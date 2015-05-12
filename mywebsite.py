70c0f5d3e31bd40f2eb9d0a44a5873302a8f59ae
fanzhong <zhongfan@lewatek.com>
2015-05-12 17:17:22 +0800

    revert

--- mywebsite.py
+++ mywebsite.py
@@ -26,7 +26,6 @@ def index():
 
 @app.route("/qiche")
 def qiche():
-    get_from_s3()
     testdata=[]
     try:
         with open("getjson.json") as jsf:
@@ -41,5 +40,5 @@ def qiche():
     return render_template('qiche.html' , testdata=testdata)
 
 if __name__ == '__main__':
-    
+    get_from_s3()
     app.run(debug = True)
